225db2e333f3710f1f1ea753ec3225515819cc5f
larrywongkahei <makemak123@gmail.com>
2024-10-23 02:09:37 +0100

    Fixed duplicate bugs

--- database_controller.js
+++ database_controller.js
@@ -2,10 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const argon2 = require('argon2');
 const uuid = require('uuid');
+const axios = require('axios');
 
 // To check if user exist or duplicate user name
 function checkIfUserFolderExist(name){
     return fs.existsSync(path.join(__dirname, "data", name));
+}
+
+function getDogsByUserName(name){
+    return JSON.parse(fs.readFileSync(path.join(__dirname, "data", name, "dogs.json")));
 }
 
 function readUserAuth(name){
@@ -61,6 +66,11 @@ async function createUser(userAuth){
             return dataToReturn;
         }
     });
+
+    // Write default dogs data in dogs.json
+    // const dogs = JSON.parse(fs.readFileSync(path.join(__dirname, "formattedDogs.json")));
+    const dogs = await formatDefaultJSON(); 
+    saveJSONFile(dogs, userAuth.username);
     
     dataToReturn.success = true;
     dataToReturn.description = "Successfully registered"
@@ -106,37 +116,135 @@ async function getHashedPassword(password){
 
 // Function to be called within two format json functions, will take in a arg object
 // I found that to call sub breed would need to use /breed/Breed/Sub-breed by checking the request url dog.ceo sent with fetching data
-function formatJSON(data){
+// function formatJSON(data){
+//     let formattedDogList = [];
+//     let shadowData = {...data};
+//     for(key of Object.keys(shadowData)){
+//         let dataToAdd = {};
+//         dataToAdd.name = key;
+//         dataToAdd.sub_breed = [];
+//         // Check if dog has sub-breed
+//         if(shadowData[key].length > 1){
+//             let subData = {};
+//             shadowData[key].forEach((each) => {
+//                 subData.name = each;
+//                 subData.imagePath = `https://dog.ceo/api/breed/${key}/${each}/images/random`;
+//                 dataToAdd.sub_breed.push(subData);
+//             })
+//         };
+//         dataToAdd.imagePath = `https://dog.ceo/api/breed/${key}/images/random`
+//         formattedDogList.push(dataToAdd);
+//     }
+//     return formattedDogList;
+// }
+
+// Function to get all image by name ( wait two one after every request to prevent exceed limit or added load to website)
+
+async function formatJSON (data)  {
     let formattedDogList = [];
     let shadowData = {...data};
     for(key of Object.keys(shadowData)){
+        console.log('========' + key + '========')
         let dataToAdd = {};
         dataToAdd.name = key;
         dataToAdd.sub_breed = [];
         // Check if dog has sub-breed
-        if(shadowData[key].length > 1){
-            let subData = {};
-            shadowData[key].forEach((each) => {
+        if(shadowData[key].length > 0){
+            for(const each of shadowData[key]){
+                let subData = {};
+                console.log(each);
                 subData.name = each;
-                subData.imagePath = `https://dog.ceo/api/breed/${key}/${each}/images/random`;
-                dataToAdd.sub_breed.push(subData);
-            })
+                try{
+                    const {data} = await axios.get(`https://dog.ceo/api/breed/${key}/${each}/images/random`);
+                    const {message} = data;
+                    subData.imagePath = message;
+                    dataToAdd.sub_breed.push(subData);
+                    console.log(message);
+                    await new Promise(resolve => setTimeout(resolve,500));
+                }catch(error){
+                    console.log("Error ---------")
+                }
+            }
         };
-        dataToAdd.imagePath = `https://dog.ceo/api/breed/${key}/images/random`
-        formattedDogList.push(dataToAdd);
+        console.log("Still running");
+        try{
+            const {data} = await axios.get(`https://dog.ceo/api/breed/${key}/images/random`);
+            const {message} = data;
+            console.log(message);
+            dataToAdd.imagePath = message
+            formattedDogList.push(dataToAdd);
+            await new Promise(resolve => setTimeout(resolve,500));
+        }catch(error){
+            console.log('Error =======')
+        }
     }
+    console.log('finished')
     return formattedDogList;
+};
 
+
+
+
+//TO TEST
+async function updateFetchedData(dogName, userName){
+    const formattedDogName = dogName.split(" ");
+    if(formattedDogName.length > 1){
+        const fName = formattedDogName[0];
+        const bName = formattedDogName[1];
+
+        try{
+            console.log(`https://dog.ceo/api/breed/${fName}/${bName}/images/random`)
+            const {data} = await axios.get(`https://dog.ceo/api/breed/${fName}/${bName}/images/random`)
+            const {message, status} = data;
+
+            console.log(message);
+
+            const userDogs = getDogsByUserName(userName);
+            const indexOfDog = userDogs.findIndex((dog) => dog.name === fName);
+
+            const mainBreed = userDogs[indexOfDog];
+
+            const sub_breedIndex = mainBreed.sub_breed.findIndex((dog) => dog.name === bName);
+            const dogToUpdate = mainBreed.sub_breed[sub_breedIndex];
+
+            const shadow = {...dogToUpdate, imagePath: message}
+            mainBreed.sub_breed[sub_breedIndex] = shadow;
+
+            saveJSONFile(userDogs, userName);
+            //Add status
+            return;
+
+        }catch(error){
+            console.log('error show in subbreed')
+        }
+    }
+
+    try{
+        console.log(`https://dog.ceo/api/breed/${dogName}/images/random`)
+        const {data} = await axios.get(`https://dog.ceo/api/breed/${dogName}/images/random`)
+        const {message, status} = data;
+
+        console.log(message);
+
+        const userDogs = getDogsByUserName(userName);
+        const indexOfDog = userDogs.findIndex((dog) => dog.name === dogName);
+
+        const dogToUpdate = userDogs[indexOfDog];
+
+        const shadow = {...dogToUpdate, imagePath: message}
+        userDogs[indexOfDog] = shadow;
+
+        saveJSONFile(userDogs, userName);
+        //Add status
+        return;
+    }catch(error){
+        console.log('error main breed')
+    }
 }
 
-async function formatUploadedJSON(jsonString){
-    const data = JSON.parse(jsonString);
-    return data;
-}
-
-function formatDefaultJSON(){
+async function formatDefaultJSON(){
     const data = JSON.parse(fs.readFileSync(path.join(__dirname, "dogs.json")));
-    return data;
+    return await formatJSON(data);
 
 }
 
@@ -150,6 +258,7 @@ function saveJSONFile(convertedData, userName){
 
 }
 
+// Used for saving image
 function getUserDirPath(userName, filename=""){
     if(filename.length > 1){
         const extension = filename.split('.')[1];
@@ -161,8 +270,9 @@ function getUserDirPath(userName, filename=""){
 
 module.exports = {
     checkIfUserFolderExist,
-    formatUploadedJSON,
     formatDefaultJSON,
+    getDogsByUserName,
+    updateFetchedData,
     validatePassword,
     getUserDirPath,
     saveJSONFile,
--- routers/dogs_router.js
+++ routers/dogs_router.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const jwt_validator = require('../middlewares/jwt/jwt_validate');
-const { formatJSON, saveJSONFile, getUserDirPath } = require('../database_controller');
+const { formatJSON, saveJSONFile, getUserDirPath, getDogsByUserName, updateFetchedData } = require('../database_controller');
 const path = require('path');
 
 const router = express.Router();
 
 router.get('/', jwt_validator, async (req, res) => {
-    res.status(200).json({success:true, description: "got data"});
+    const data = getDogsByUserName(req.userName);
+    res.status(200).json({success:true, description: "got data", data:data});
 })
 
+router.patch('/image/:name', jwt_validator, async (req, res) => {
+    console.log("get")
+    const name = req.params.name;
+    const data = await updateFetchedData(name, req.userName);
+    res.json({data:"hi"})
+})
+
+//Allow user to upload image or json file, if user know what they are doing. Image are for icon.
 router.post('/fileUpload', jwt_validator, async (req, res) => {
     try{
         if(!req.files){
